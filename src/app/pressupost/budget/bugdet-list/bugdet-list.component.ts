b2ac8f91fc6e7d214744503d336fc3d2c5b92964
sacom1441 <samir.comas1@gmail.com>
2023-08-29 16:07:35 +0200

    sprint7 nivell2 exercici 6 estructura v.1

---
+++ src/app/pressupost/budget/bugdet-list/bugdet-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-bugdet-list',
+  templateUrl: './bugdet-list.component.html',
+  styleUrls: ['./bugdet-list.component.css']
+})
+export class BugdetListComponent {
+
+}
--- src/app/pressupost/pressupost.module.ts
+++ src/app/pressupost/pressupost.module.ts
@@ -1,24 +1,26 @@
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+
+import { BugdetListComponent } from './budget/bugdet-list/bugdet-list.component';
 import { HomeComponent } from './home/home.component';
-import { PanellComponent } from './panell/panell.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { RouterModule } from '@angular/router';
-import { PageModalComponent } from './modal/page-modal/page-modal.component';
 import { LenguageModalComponent } from './modal/lenguage-modal/lenguage-modal.component';
-
+import { PageModalComponent } from './modal/page-modal/page-modal.component';
+import { PanellComponent } from './panell/panell.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 
 @NgModule({
   declarations: [
+    BugdetListComponent,
     HomeComponent,
+    LenguageModalComponent,
+    PageModalComponent,
     PanellComponent,
     WelcomeComponent,
-    PageModalComponent,
-    LenguageModalComponent,
 
   ],
   imports: [
